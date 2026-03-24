573d965a9780d5ddfb348276adfee48ce6f65078
Jesse <jessekeskikorhonen@gmail.com>
2026-03-24 14:08:27 +0200

    erittäin alkukantainen material UI login dialog

---
+++ src/components/Login.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+
+type LoginProps = {
+	open?: boolean
+	onLogin: (username: string, password: string) => void
+}
+
+export default function Login({ open = true, onLogin }: LoginProps) {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		onLogin(username, password)
+	}
+
+	return (
+		<Dialog open={open} fullWidth maxWidth="xs">
+			<form onSubmit={handleSubmit}>
+				<DialogTitle>Kirjaudu sisään</DialogTitle>
+				<DialogContent>
+					<Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 1 }}>
+						<TextField
+							label="Käyttäjätunnus"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							autoFocus
+							fullWidth
+						/>
+						<TextField
+							label="Salasana"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							fullWidth
+						/>
+					</Box>
+				</DialogContent>
+				<DialogActions sx={{ pr: 2, pb: 2 }}>
+					<Button type="submit" variant="contained">Kirjaudu</Button>
+				</DialogActions>
+			</form>
+		</Dialog>
+	)
+}
