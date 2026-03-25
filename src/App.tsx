db4c4344be940bb39d90973f046d9cafbdd613f3
Eetu Kiljala <eetu.kiljala@student.kpedu.fi>
2026-03-25 09:55:04 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css'
-<<<<<<< HEAD
-import Navbar from './components/navbar'
-
-=======
 import Calendar from './components/Calendar.tsx'
->>>>>>> 1279ba63bb214b30dac03485790a08a4aaf245af
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <Navbar />
-=======
     <h1>Kevätprojekti</h1>
     <Calendar />
->>>>>>> 1279ba63bb214b30dac03485790a08a4aaf245af
     </>
   )
 }
