8aa278cf959dac2df0325c980167ed827e9522a1
Eetu Kiljala <eetu.kiljala@student.kpedu.fi>
2026-03-31 09:43:14 +0300

    GroupPage tiedoston importnimen korjaus

--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import { Button } from '@mui/material'
-import ClassroomFormDialog from '../components/dialogs/GroupFormDialog'
+import GroupFormDialog from '../components/dialogs/GroupFormDialog'
 
 export default function GroupPage() {
   const [open, setOpen] = useState(false)
   return (
     <>
       <Button onClick={() => setOpen(true)}>Lisää ryhmä</Button>
-      <ClassroomFormDialog open={open} onClose={() => setOpen(false)} />
+      <GroupFormDialog open={open} onClose={() => setOpen(false)} />
     </>
   )
 }
